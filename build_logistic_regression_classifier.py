39d8d4b63167530600feba899102035ba668df17
KeyiT <kytangls92@gmail.com>
2017-10-01 22:33:54 -0700

    Change 'build_train_test' function to 'train_test'. The new funtion does not initialize the target estimator anymore. The new function gets the target estimator from its first argument. Update other classifier building file accordingly.

--- build_logistic_regression_classifier.py
+++ build_logistic_regression_classifier.py
@@ -4,6 +4,7 @@ import build_classifier as bc
 if __name__ == "__main__":
 
     Cs = np.arange(0.1, 2.1, 0.1)
-    bc.build_train_test(
-        bc.build_lr, [Cs]
+
+    bc.train_test(
+        bc.build_lr(), {'C': Cs}
     )
